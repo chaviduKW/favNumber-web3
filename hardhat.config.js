78b083712411903423f842f38e45281dd9d91dc2
Chavidu <chavidukalpana40@gmail.com>
2023-10-28 22:53:11 +0530

    deploy the contract on sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
-require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks:{
+    sepolia:{
+      url: process.env.ALCHEMY_URL,
+      accounts:[process.env.ACCOUNT_KEY]
+    }
+  }
 };
